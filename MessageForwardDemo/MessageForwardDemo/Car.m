124086150e5cb18d330bdac8240660d0a1ae7cf6
reborn <343633674@qq.com>
2020-12-30 11:13:54 +0800

    新增内容

---
+++ MessageForwardDemo/MessageForwardDemo/Car.m
@@ -0,0 +1,17 @@
+//
+//  Car.m
+//  MessageForwardDemo
+//
+//  Created by reborn on 2020/8/27.
+//  Copyright © 2020 reborn. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+- (void)run {
+    NSLog(@"car run");
+}
+
+@end
---
+++ MessageForwardDemo/MessageForwardDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MessageForwardDemo
+//
+//  Created by reborn on 2020/8/27.
+//  Copyright © 2020 reborn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
